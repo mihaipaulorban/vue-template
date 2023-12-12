c336ee899cea8d6e7c1d98d61accb7167830e8d2
mihaipaulorban <mihaipaulorban@gmail.com>
2023-12-12 12:35:43 +0100

    feat: style files

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,8 @@ export default {
 
 <template></template>
 
-<style></style>
+<style scoped lang="scss">
+@use './assets/scss/partials/variables' as *;
+@use './assets/scss/partials/common ' as *;
+@use './assets/scss/partials/reset' as *;
+</style>
